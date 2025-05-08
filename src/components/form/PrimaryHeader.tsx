e42ff0debc12d4fc27234ea72cf68a1849c94d8a
Jihad Zaidan <127509175+JihadZaidan@users.noreply.github.com>
2025-05-08 09:41:50 +0700

    Add files via upload

---
+++ src/components/form/PrimaryHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PrimaryHeader() {
+    return(
+        <div className="w-full h-full gap-4">
+            <h1 className="font-sans font-semibold leading-5 w-120 h-34 ">Hello World !</h1>
+            <text className=""></text>
+        </div>
+    )
+}
